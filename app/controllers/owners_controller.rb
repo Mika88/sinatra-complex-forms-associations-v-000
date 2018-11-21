c11193f764eefbfc4e701aaece9ffe34fa54ba94
Mika88 <mikaspivak@gmail.com>
2018-11-21 21:37:49 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -31,7 +31,7 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    
+
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
